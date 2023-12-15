172a90c54ff32ba4ad2a037384ca3bd842a19d6e
Manuel Lopera <manuellopera612@gmail.com>
2023-12-14 19:11:40 -0500

    init project

---
+++ src/test/java/com/pragma/cuddlylamp/CuddlyLampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.cuddlylamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuddlyLampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
